d3764c56946073b53c6fb78ffccd87fd7ebb49eb
Blacklisteddeveloper <blacklisted.web02@gmail.com>
2018-03-20 16:29:06 +0530

    commands works

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -1,8 +1,20 @@
-(function($) {
+/**(function($) {
 	// Site title
 	wp.customize('blogname', function(value) {
 		value.bind(function(to) {
 			$('.brand').text(to);
 		});
 	});
-})(jQuery);
+})(jQuery);*/
+
+/** For Handberber Start*/
+    var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+    }
+/** For Handberber End*/
