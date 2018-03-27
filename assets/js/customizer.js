7c3bc26de18c7cfd296c81fbeb4b8a7db1d47ec9
Blacklisteddeveloper <blacklisted.web02@gmail.com>
2018-03-27 16:56:23 +0530

    home

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -10,10 +10,13 @@
 /** For Handberber Start*/
     var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
     var hamburgers = document.querySelectorAll(".hamburger");
+	 jQuery(".hamburgerbox").hide();
     if (hamburgers.length > 0) {
       forEach(hamburgers, function(hamburger) {
         hamburger.addEventListener("click", function() {
 		 jQuery(".hamburgerbox").toggle();
+		 jQuery(".hamburgerbox").toggleClass('fadeIn');
+		 jQuery(".hamburgerbox").toggleClass('fadeOut');
 		this.classList.toggle("is-active");
 
         }, false);
